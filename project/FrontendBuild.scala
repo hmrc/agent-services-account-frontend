8e95cfedb090cc16e851550b4682c6da36fc573c
Artur Opala <artur.opala@siili.fi>
2017-11-06 17:05:55 +0000

    [APB-1450][AO] remove commented out code and unnecessary plugin

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,29 +11,6 @@ object FrontendBuild extends Build with MicroService {
 
   override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
 
-  /*val compile = Seq(
-    ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.26.0",
-    "uk.gov.hmrc" %% "play-partials" % "5.4.0",
-    "uk.gov.hmrc" %% "play-config" % "4.3.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.3.0",
-    "uk.gov.hmrc" %% "play-health" % "2.1.0",
-    "uk.gov.hmrc" %% "play-ui" % "7.5.0",
-    "uk.gov.hmrc" %% "play-events" % "1.2.0",
-    "uk.gov.hmrc" %% "play-auth" % "2.1.0",
-    "uk.gov.hmrc" %% "agent-mtd-identifiers" % "0.5.0"
-  )
-
-  def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.scalatest" %% "scalatest" % "3.0.0" % scope,
-    "org.mockito" % "mockito-core" % "2.8.9" % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
-    "com.github.tomakehurst" % "wiremock" % "2.3.1" % scope
-  )*/
-
   lazy val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "frontend-bootstrap" % "8.10.0",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -11,5 +11,3 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.0.0")
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.12")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
-
-addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.5.3")
