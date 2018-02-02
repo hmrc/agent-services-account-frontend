c8bcbdcef73fa19443d97d52dadc01528ceebd98
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2018-02-02 16:27:47 +0000

    [AFID-304] Google Analytics - Agents for Individuals Event Tracking

--- test/uk/gov/hmrc/agentservicesaccount/controllers/AgentServicesControllerSpec.scala
+++ test/uk/gov/hmrc/agentservicesaccount/controllers/AgentServicesControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,9 +125,11 @@ class AgentServicesControllerSpec extends WordSpec with Matchers with OptionValu
       content should include(messagesApi("agent.services.account.additional.links.mapping.body1"))
       content should include(messagesApi("agent.services.account.additional.links.mapping.body2", mappingUrl, "agentMappingLinkId"))
       content should include(messagesApi("agent.manageClients"))
-      content should include(messagesApi("agent.invitations.links.start", invitationsUrl, "agentInvitationsLinkId"))
+      content should include(messagesApi("agent.invitations.links.start", invitationsUrl, "agentInvitationsLinkId",
+        "ga('send', 'event', 'Manage your clients', 'Click', 'Request authorisation to view an individual’s data')"))
       content should include(htmlEscapedMessage(messagesApi("agent.services.account.additional.links.agent-afi.body1")))
-      content should include(messagesApi("agent.services.account.additional.links.agent-afi.body2", agentAfiUrl, "agentAfiLinkId"))
+      content should include(messagesApi("agent.services.account.additional.links.agent-afi.body2", agentAfiUrl, "agentAfiLinkId",
+        "ga('send', 'event', 'Services you might need', 'Click', 'View a client’s income record')"))
       content should include(arn)
       content should include(signOutUrl)
       content should include(mappingUrl)
