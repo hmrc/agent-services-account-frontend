9171da1d710c22aa778c37a4008edb96508f2828
Marianne <32272884+mariannepearson0@users.noreply.github.com>
2019-10-15 11:25:59 +0100

    [APB-4435][MP] Replace email link with a link to a deskpro form (#135)
    * [APB-4435][MP] Change accessibility statement to link to deskpro instead of emailing
    
    * [APB-4435][MP] adding quotes
    
    * [APB-4435][MP] removing duplicated config
    
    * [APB-4435][MP] adding test for accessibility controller
    
    * [APB-4435][MP] making get a getOrElse

---
+++ test/uk/gov/hmrc/agentservicesaccount/controllers/AccessibilityStatementControllerSpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentservicesaccount.controllers
+
+import play.api.i18n.MessagesApi
+import play.api.test.FakeRequest
+import uk.gov.hmrc.agentservicesaccount.support.BaseUnitSpec
+import play.api.test.Helpers._
+import play.mvc.Http.HeaderNames
+
+class AccessibilityStatementControllerSpec extends BaseUnitSpec {
+
+  val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+  val controller: AccessibilityStatementController = new AccessibilityStatementController(messagesApi)
+
+  "display the accessibility statement with a link to contact frontend to report problem" in {
+    val result = controller.showAccessibilityStatement()(FakeRequest().withHeaders(HeaderNames.REFERER -> "foo"))
+    val content = contentAsString(result)
+
+    status(result) shouldBe 200
+    content should include(messagesApi("accessibility.statement.h1"))
+    content should include("http://localhost:9250/contact/accessibility?service=AOSS&userAction=foo")
+  }
+}
