ccd2a4387c40bd63a5f60999518a209c4c911f83
Unmesh Malvankar <unmesh.malvankar1@digital.hmrc.gov.uk>
2022-09-13 13:03:27 +0100

    [APB-6271][UM] Connector for fetching user details

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,7 +3,7 @@ resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 
 addSbtPlugin("com.typesafe.sbt"  % "sbt-twirl" 		% "1.5.1")
-addSbtPlugin("com.typesafe.play" % "sbt-plugin"         % "2.8.13")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin"         % "2.8.16")
 
 addSbtPlugin("uk.gov.hmrc"       % "sbt-auto-build"     % "3.8.0")
 addSbtPlugin("uk.gov.hmrc"       % "sbt-distributables" % "2.1.0")
---
+++ test/uk/gov/hmrc/agentservicesaccount/stubs/UserDetailsStubs.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentservicesaccount.stubs
+
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, stubFor, urlEqualTo}
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.libs.json.Json
+import uk.gov.hmrc.agentservicesaccount.models.UserDetails
+
+object UserDetailsStubs {
+
+  def givenUserDetailsFetchedOk(userId: String, getUserDetails: UserDetails): StubMapping =
+    stubFor(
+      get(urlEqualTo(s"/users-groups-search/users/$userId"))
+        .willReturn(aResponse()
+          .withStatus(200)
+          .withBody(Json.toJson(getUserDetails).toString)))
+
+  def givenUserDetailsNotFound(userId: String): StubMapping =
+    stubFor(
+      get(urlEqualTo(s"/users-groups-search/users/$userId"))
+        .willReturn(aResponse()
+          .withStatus(400)
+          .withBody("User details not found")))
+
+  def givenServerErrorWhenFetchingUserDetails(userId: String): StubMapping =
+    stubFor(
+      get(urlEqualTo(s"/users-groups-search/users/$userId"))
+        .willReturn(aResponse()
+          .withStatus(500)
+          .withBody("Backend error")))
+
+
+}
