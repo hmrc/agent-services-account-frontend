14d470443e475cbe13afb454aa62bcedb5461630
Caroline Strasenburgh <62263396+CarolineS-QA@users.noreply.github.com>
2023-02-09 17:26:18 +0000

    APB-6908 [CS] update standard user link (#339)
    * APB-6908 [CS] update standard user link
    
    * APB-6908 [CS] remove unneeded dep
    
    * APB-6908 [CS] update spec
    
    * APB-6908 [CS] update spec

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,18 +7,18 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "7.13.0",
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "4.8.0-play-28",
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "0.57.0-play-28",
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "0.60.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVer,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "5.5.0-play-28"
   )
 
   val test = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "5.1.0"    % "test",
-    "org.scalatestplus"      %% "mockito-3-12"               % "3.2.10.0" % "test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"    % mongoVer   % "test, it",
-    "com.github.tomakehurst" %  "wiremock-jre8"              % "2.26.1"   % "test, it",
-    "org.jsoup"              %  "jsoup"                      % "1.15.3"   % "test, it",
-    "com.vladsch.flexmark"   %  "flexmark-all"               % "0.35.10"  % "test, it"
+    "org.scalatestplus.play" %% "scalatestplus-play"         % "5.1.0"      % "test",
+    "org.scalatestplus"      %% "mockito-3-12"               % "3.2.10.0"   % "test",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"    % mongoVer     % "test, it",
+    "com.github.tomakehurst" %  "wiremock-jre8"              % "2.26.1"     % "test, it",
+    "org.jsoup"              %  "jsoup"                      % "1.15.3"     % "test, it",
+    "com.vladsch.flexmark"   %  "flexmark-all"               % "0.35.10"    % "test, it"
   )
 
 }
