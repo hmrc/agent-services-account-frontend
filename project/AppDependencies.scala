95ad3b5c398c5aadcf2a0e87d09d27987ed6f16b
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-02-09 15:46:59 +0000

    APB-6908 [CS] update standard user link

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,25 @@ import sbt._
 
 object AppDependencies {
   private val mongoVer: String = "0.74.0"
+  private val bootstrapVer: String = "7.13.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "7.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVer,
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "4.8.0-play-28",
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "0.57.0-play-28",
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "0.60.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVer,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "5.5.0-play-28"
   )
 
   val test = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "5.1.0"    % "test",
-    "org.scalatestplus"      %% "mockito-3-12"               % "3.2.10.0" % "test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"    % mongoVer   % "test, it",
-    "com.github.tomakehurst" %  "wiremock-jre8"              % "2.26.1"   % "test, it",
-    "org.jsoup"              %  "jsoup"                      % "1.15.3"   % "test, it",
-    "com.vladsch.flexmark"   %  "flexmark-all"               % "0.35.10"  % "test, it"
+    "org.scalatestplus.play" %% "scalatestplus-play"         % "5.1.0"      % "test",
+    "org.scalatestplus"      %% "mockito-3-12"               % "3.2.10.0"   % "test",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"    % mongoVer     % "test, it",
+    "uk.gov.hmrc"    %% "uk.gov.hmrc.bootstrap-test-play-28" % bootstrapVer % "test, it",
+    "com.github.tomakehurst" %  "wiremock-jre8"              % "2.26.1"     % "test, it",
+    "org.jsoup"              %  "jsoup"                      % "1.15.3"     % "test, it",
+    "com.vladsch.flexmark"   %  "flexmark-all"               % "0.35.10"    % "test, it"
   )
 
 }
