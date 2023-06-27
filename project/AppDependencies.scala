1c225679f243cce45dda589f0f147af72ac4e2a2
raw1697 <romel.w@hotmail.co.uk>
2023-06-27 14:27:04 +0100

    APB 7238 - Updated libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object AppDependencies {
-  private val mongoVer: String = "1.2.0"
-  private val bootstrapVer: String = "7.15.0"
+  private val mongoVer: String = "1.3.0"
+  private val bootstrapVer: String = "7.19.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVer,
     "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "5.3.0",
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.2.0",
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.10.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVer,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.7.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.13.0-play-28"
   )
 
   val test = Seq(
