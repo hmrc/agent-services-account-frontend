65ad8bf7ab47f8a39c78e10a9f136e520798d588
Geoff Watson <9043337+geoffreywatson@users.noreply.github.com>
2024-03-15 16:56:35 +0000

    Apb 7896 (#438)
    * [APB-7896][GW] enter registration number page
    
    * some welsh included

---
+++ app/uk/gov/hmrc/agentservicesaccount/controllers/amls/EnterRegistrationNumberController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentservicesaccount.controllers.amls
+
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.agentservicesaccount.actions.Actions
+import uk.gov.hmrc.agentservicesaccount.config.AppConfig
+import uk.gov.hmrc.agentservicesaccount.controllers.ToFuture
+import uk.gov.hmrc.agentservicesaccount.forms.{NewRegistrationNumberForm}
+import uk.gov.hmrc.agentservicesaccount.repository.UpdateAmlsJourneyRepository
+import uk.gov.hmrc.agentservicesaccount.views.html.pages.amls.enter_registration_number
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+
+@Singleton
+class EnterRegistrationNumberController @Inject()(actions: Actions,
+                                           val updateAmlsJourneyRepository: UpdateAmlsJourneyRepository,
+                                           enterRegistrationNumber: enter_registration_number,
+                                           cc: MessagesControllerComponents
+                                          )(implicit appConfig: AppConfig, ec: ExecutionContext) extends FrontendController(cc) with AmlsJourneySupport with I18nSupport {
+
+  private def registrationNumberForm(isHmrc: Boolean) = NewRegistrationNumberForm.form(isHmrc)
+
+  def showPage: Action[AnyContent] = actions.authActionCheckSuspend.async { implicit request =>
+    actions.ifFeatureEnabled(appConfig.enableNonHmrcSupervisoryBody) {
+      withUpdateAmlsJourney { amlsJourney =>
+        val form = amlsJourney
+          .newMembershipNumber
+          .fold(registrationNumberForm(amlsJourney.isHmrc))(registrationNumberForm(amlsJourney.isHmrc).fill)
+        Ok(enterRegistrationNumber(form)).toFuture
+      }
+    }
+  }
+
+
+  def onSubmit: Action[AnyContent] = actions.authActionCheckSuspend.async { implicit request =>
+    actions.ifFeatureEnabled(appConfig.enableNonHmrcSupervisoryBody) {
+      withUpdateAmlsJourney { amlsJourney =>
+        registrationNumberForm(amlsJourney.isHmrc)
+          .bindFromRequest()
+          .fold(
+            formWithError => Ok(enterRegistrationNumber(formWithError)).toFuture,
+            data =>
+              saveAmlsJourney(amlsJourney.copy(newMembershipNumber = Option(data))).map(_ => {
+                val nextPage = if (amlsJourney.isUkAgent) routes.EnterRenewalDateController.showPage.url
+                else "/not-implemented"
+                Redirect(nextPage)
+              }
+              )
+          )
+      }
+    }
+  }
+}
+
---
+++ test/uk/gov/hmrc/agentservicesaccount/forms/NewRegistrationNumberFormSpec.scala
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentservicesaccount.forms
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class NewRegistrationNumberFormSpec extends AnyWordSpec with Matchers {
+
+
+  "NewRegistrationNumberForm binding" should {
+    "for HMRC AMLS" when {
+
+      "succeed when valid data provided" in {
+
+        val data = Map(
+          "number" -> "XAML00000123456"
+        )
+
+        val result = NewRegistrationNumberForm.form(isHmrc = true).bind(data)
+
+        result.value shouldBe Some("XAML00000123456")
+      }
+
+      "fail when no data provided" in {
+        val data = Map(
+          "number" -> ""
+        )
+
+        val result = NewRegistrationNumberForm.form(isHmrc = true).bind(data)
+
+        result.value shouldBe None
+        result.errors.size shouldBe 1
+        result.errors.head.message shouldBe "amls.enter-registration-number.error.empty"
+
+      }
+
+      "fail when invalid data provided" in {
+        val data = Map(
+          "number" -> "GGG12345X"
+        )
+
+        val result = NewRegistrationNumberForm.form(isHmrc = true).bind(data)
+
+        result.value shouldBe None
+        result.errors.size shouldBe 1
+        result.errors.head.message shouldBe "amls.enter-registration-number.error.hmrc.invalid"
+      }
+    }
+
+    "for not HMRC AMLS" when {
+
+      "succeed when valid data provided" in {
+
+        val data = Map(
+          "number" -> "GGG12345X"
+        )
+
+        val result = NewRegistrationNumberForm.form(isHmrc = false).bind(data)
+
+        result.value shouldBe Some("GGG12345X")
+      }
+
+      "fail when no data provided" in {
+        val data = Map(
+          "number" -> ""
+        )
+
+        val result = NewRegistrationNumberForm.form(isHmrc = false).bind(data)
+
+        result.value shouldBe None
+        result.errors.size shouldBe 1
+        result.errors.head.message shouldBe "amls.enter-registration-number.error.empty"
+      }
+
+      "fail when invalid data provided" in {
+        val data = Map(
+          "number" -> "%$%1234FF"
+        )
+
+        val result = NewRegistrationNumberForm.form(isHmrc = false).bind(data)
+
+        result.value shouldBe None
+        result.errors.size shouldBe 1
+        result.errors.head.message shouldBe "amls.enter-registration-number.error.not-hmrc.invalid"
+      }
+    }
+  }
+
+  }
