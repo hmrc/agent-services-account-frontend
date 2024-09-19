1f27beb2fd34dd8b3305da7c5278d135b64ce003
Artemas Muzanenhamo <artemasseng@gmail.com>
2024-09-19 09:12:24 +0100

    APB-8746: Bump up play-frontend-hmrc to v9.0.0 (#514)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 object AppDependencies {
   private val mongoVersion: String = "1.9.0"
   private val bootstrapVersion: String = "8.6.0"
-  private val enumeratumVersion = "1.8.0"
+  private val enumeratumVersion = "1.8.1"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-partials-play-30" % "9.1.0",
     "uk.gov.hmrc" %% "agent-mtd-identifiers" % "1.15.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % mongoVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.5.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "9.0.0",
     "com.beachape" %% "enumeratum-play" % enumeratumVersion,
     "org.julienrf" %% "play-json-derived-codecs" % "11.0.0",
     "org.apache.commons" % "commons-text" % "1.12.0",
@@ -19,7 +19,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVersion % Test,
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.31" % Test,
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.37" % Test,
     "org.scalamock" %% "scalamock" % "6.0.0" % Test
   )
 
