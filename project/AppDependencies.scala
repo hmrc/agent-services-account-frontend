bfa0117ca878ff8184dde87314dee5aa62aab77b
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-06-27 15:05:32 +0100

    APB-9620 Govuk rebrand (#528)
    * APB-9620 use Govuk rebrand
    
    * APB-9620 remove unused and conflicting sass

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,17 @@ import sbt.*
 object AppDependencies {
   private val mongoVersion: String = "2.6.0"
   private val bootstrapVersion: String = "9.13.0"
-  private val enumeratumVersion = "1.8.1"
+  private val enumeratumVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.0.0",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.2.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.6.0",
     "com.beachape"      %% "enumeratum-play"            % enumeratumVersion,
     "org.julienrf"      %% "play-json-derived-codecs"   % "11.0.0",
-    "org.apache.commons" % "commons-text"               % "1.12.0",
+    "org.apache.commons" % "commons-text"               % "1.13.1",
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.2.0"
   )
 
