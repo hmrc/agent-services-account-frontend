3743326c41a59b29f9d79cb08f1620c007db0b5d
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-07-02 15:26:07 +0100

    APB-9645 dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.0.0",
+    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.1.0",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.2.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.6.0",
