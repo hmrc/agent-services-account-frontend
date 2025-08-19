a53f9b57f5c8219cd870d5a0f37599a77e146e69
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-08-19 16:46:14 +0100

    APB-8273 fix missing vals

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,26 +1,27 @@
 import sbt.*
 
 object AppDependencies {
-
-  val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-partials-play-30" % "10.1.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % mongoVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.8.0",
-    "com.beachape" %% "enumeratum-play" % enumeratumVersion,
-    "org.julienrf" %% "play-json-derived-codecs" % "11.0.0",
-    "org.apache.commons" % "commons-text" % "1.14.0",
-    "uk.gov.hmrc" %% "crypto-json-play-30" % "8.3.0",
-    "uk.gov.hmrc" %% "domain-play-30" % "11.0.0"
-  )
-  val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVersion % Test,
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "2.0.0" % Test,
-    "org.scalamock" %% "scalamock" % "7.4.1" % Test
-  )
   private val mongoVersion: String = "2.7.0"
   private val bootstrapVersion: String = "10.1.0"
   private val enumeratumVersion = "1.9.0"
+
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.1.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "com.beachape"      %% "enumeratum-play"            % enumeratumVersion,
+    "org.julienrf"      %% "play-json-derived-codecs"   % "11.0.0",
+    "org.apache.commons" % "commons-text"               % "1.14.0",
+    "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.3.0",
+    "uk.gov.hmrc"       %% "domain-play-30"      % "11.0.0"
+  )
+
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"    % mongoVersion      % Test,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"     % bootstrapVersion  % Test,
+    "org.mockito"       %% "mockito-scala-scalatest"    % "2.0.0"           % Test,
+    "org.scalamock"     %% "scalamock"                  % "7.4.1"           % Test
+  )
 
 }
