010cf514d9180e160653d77e96a133b39e3daf28
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-12-08 09:59:37 +0000

    APB-10210: Addresses PlatOpsPRBot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,14 @@
 import sbt.*
 
 object AppDependencies {
-  private val mongoVersion: String = "2.10.0"
+  private val mongoVersion: String = "2.11.0"
   private val bootstrapVersion: String = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.21.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.22.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0",
     "com.beachape"      %% "enumeratum-play"            % "1.9.0",
