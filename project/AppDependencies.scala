258e559acc27350b87efb29c1601afe07aca463d
soloshah <38477922+soloshah@users.noreply.github.com>
2026-01-16 16:46:53 +0000

    updated dependencies related to Pr

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,19 +8,19 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.25.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.26.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0",
-    "com.beachape"      %% "enumeratum-play"            % "1.9.0",
+    "com.beachape"      %% "enumeratum-play"            % "1.9.2",
     "org.julienrf"      %% "play-json-derived-codecs"   % "11.0.0",
-    "org.apache.commons" % "commons-text"               % "1.14.0"
+    "org.apache.commons" % "commons-text"               % "1.15.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"    % mongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"     % bootstrapVersion,
     "org.mockito"       %% "mockito-scala-scalatest"    % "2.0.0",
-    "org.scalamock"     %% "scalamock"                  % "7.5.0"
+    "org.scalamock"     %% "scalamock"                  % "7.5.3"
   ).map(_ % Test)
 
 }
