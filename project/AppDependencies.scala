dbe3047bb25443e7184974d6a9028aeced962b2c
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-02-02 16:59:33 +0000

    APB-10589: Adds playVersion private val in AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,24 +3,25 @@ import sbt.*
 object AppDependencies {
   private val mongoVersion: String = "2.11.0"
   private val bootstrapVersion: String = "10.5.0"
+  private val playVersion: String = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
-    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.26.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0",
-    "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0",
-    "com.beachape"      %% "enumeratum-play"            % "1.9.0",
-    "org.julienrf"      %% "play-json-derived-codecs"   % "11.0.0",
-    "org.apache.commons" % "commons-text"               % "1.14.0"
+    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % mongoVersion,
+    "uk.gov.hmrc"       %% s"play-partials-$playVersion"      % "10.2.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.26.0",
+    "uk.gov.hmrc"       %% s"crypto-json-$playVersion"        % "8.4.0",
+    "uk.gov.hmrc"       %% s"domain-$playVersion"             % "11.0.0",
+    "com.beachape"      %% "enumeratum-play"                  % "1.9.0",
+    "org.julienrf"      %% "play-json-derived-codecs"         % "11.0.0",
+    "org.apache.commons" % "commons-text"                     % "1.14.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"    % mongoVersion,
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "org.mockito"       %% "mockito-scala-scalatest"    % "2.0.0",
-    "org.scalamock"     %% "scalamock"                  % "7.5.0"
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % mongoVersion,
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
+    "org.mockito"       %% "mockito-scala-scalatest"       % "2.0.0",
+    "org.scalamock"     %% "scalamock"                     % "7.5.0"
   ).map(_ % Test)
 
 }
