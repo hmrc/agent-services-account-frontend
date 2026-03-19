09eae678450c65547ffa1b59aa566859a8c6ef36
kaizar12 <10980326+kaizar12@users.noreply.github.com>
2026-03-19 11:42:05 +0000

    APB-10902 upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
   private val mongoVersion: String = "2.12.0"
-  private val bootstrapVersion: String = "10.5.0"
+  private val bootstrapVersion: String = "10.7.0"
   private val playVersion: String = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % mongoVersion,
     "uk.gov.hmrc"       %% s"play-partials-$playVersion"                 % "10.2.0",
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.31.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.32.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.4.0",
     "uk.gov.hmrc"       %% s"crypto-json-$playVersion"                   % "8.4.0",
     "uk.gov.hmrc"       %% s"domain-$playVersion"                        % "11.0.0",
