945145dbcb50b121ed56c55a7d9c94dbf8396081
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-04-16 16:17:25 +0100

    APB-11053: Fixes import in UpdateAddressControllerISpec

--- test/it/controllers/subscriptions/UpdateAddressControllerISpec.scala
+++ test/it/controllers/subscriptions/UpdateAddressControllerISpec.scala
@@ -22,6 +22,7 @@ import stubs.AgentServicesAccountStubs.stubASAGetResponseError
 import support.ComponentBaseISpec
 import uk.gov.hmrc.agentservicesaccount.controllers.subscriptionJourneyKey
 import uk.gov.hmrc.agentservicesaccount.controllers.subscriptions
+import uk.gov.hmrc.agentservicesaccount.models.subscriptions.LegacyRegime
 import uk.gov.hmrc.agentservicesaccount.models.subscriptions.LegacyRegime.CT
 import uk.gov.hmrc.agentservicesaccount.models.subscriptions.LegacyRegime.SA
 import uk.gov.hmrc.agentservicesaccount.repository.SessionCacheRepository
