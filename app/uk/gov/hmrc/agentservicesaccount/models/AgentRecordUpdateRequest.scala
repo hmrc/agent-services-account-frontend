6b34aadcda4582e934fc51b68a356434ba385b4f
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-04-21 14:17:08 +0100

    APB-10862: Removes erroneous import

--- app/uk/gov/hmrc/agentservicesaccount/models/AgentRecordUpdateRequest.scala
+++ app/uk/gov/hmrc/agentservicesaccount/models/AgentRecordUpdateRequest.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.agentservicesaccount.models
 
-import play.api.libs.json.{*, Format, Json}
+import play.api.libs.json.{Format, Json}
 
 case class AgentRecordUpdateRequest(
 //                                     amlsDetails: Option[AmlsDetails],
