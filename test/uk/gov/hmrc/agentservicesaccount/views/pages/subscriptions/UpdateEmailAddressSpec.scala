28f6dedd77ec5da9dd1bbfabe1edf150d4c512df
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-07-23 16:00:07 +0100

    APB-11839: Removes no longer needed TODO in view spec

--- test/uk/gov/hmrc/agentservicesaccount/views/pages/subscriptions/UpdateEmailAddressSpec.scala
+++ test/uk/gov/hmrc/agentservicesaccount/views/pages/subscriptions/UpdateEmailAddressSpec.scala
@@ -29,7 +29,6 @@ import uk.gov.hmrc.agentservicesaccount.models.subscriptions.LegacyRegime
 import uk.gov.hmrc.agentservicesaccount.views.ViewBaseSpec
 import uk.gov.hmrc.agentservicesaccount.views.html.pages.subscriptions.update_email_address
 
-//    TODO: 11839 Add tests relating to isEmailVerified true/false
 class UpdateEmailAddressSpec
 extends ViewBaseSpec {
 
